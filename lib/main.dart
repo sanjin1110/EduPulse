5714a5907549d99e57ef394a017cacd86ab5b6cf
sanjin1110 <thapasanjin00@gmail.com>
2024-02-09 21:13:22 +0545

    online learning application

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
-import 'package:edupulse/login.dart';
-import 'package:edupulse/registration.dart';
+import 'package:education_app/screens/base_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,18 +6,39 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'EduPulse',
       debugShowCheckedModeBanner: false,
+      title: 'EduPulse',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
+        fontFamily: 'Poppins',
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            fontSize: 20,
+            color: Colors.white,
+            fontWeight: FontWeight.w500,
+          ),
+          bodyLarge: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
+          ),
+          bodyMedium: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+          ),
+          displayMedium: TextStyle(
+            fontWeight: FontWeight.w700,
+            fontSize: 18,
+            color: Colors.black,
+          ),
+        ),
       ),
-      home: const LoginWidget(),
+      home: const BaseScreen(),
     );
   }
 }
